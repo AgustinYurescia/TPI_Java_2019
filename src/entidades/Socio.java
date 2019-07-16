771ba6ef98168c6f475133e37e971b5eb71d48f4
Gianluca <Gianluca@DESKTOP-37S0GM7>
2019-07-16 17:55:24 -0300

    agregado de clases TP anterior

---
+++ src/entidades/Socio.java
@@ -0,0 +1,13 @@
+package entidades;
+
+public class Socio extends Cliente {
+	private Double descuentoSocio;
+
+	public Double getDescuentoSocio() {
+		return descuentoSocio;
+	}
+
+	public void setDescuentoSocio(Double descuentoSocio) {
+		this.descuentoSocio = descuentoSocio;
+	}
+}
