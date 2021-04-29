7e30864b206b110db3c8e11293191ed684dcd184
AgustinYurescia <agustin_yurescia@hotmail.com>
2021-04-29 18:36:57 -0300

    Seguridad en post de controllers

--- src/controlador/ControladorProducto.java
+++ src/controlador/ControladorProducto.java
@@ -40,94 +40,14 @@ public class ControladorProducto extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String acceso = "";
 		String action = request.getParameter("accion");
-	if(action.equalsIgnoreCase("mostrar_producto")) 
-	{
-		Producto prod;
-		try
-		{
-			prod = _servicioProducto.GetProducto(Integer.parseInt(request.getParameter("codigo_producto")));
-			request.setAttribute("producto", prod);
-		}
-		catch (Exception e)
-		{
-			request.setAttribute("mensajeError", "Error interno del servidor");
-		}
-		acceso = "producto.jsp";
-	}
-	else if(action.equalsIgnoreCase("listarCliente")) 
-	{
-		request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
-		acceso="listarProductos.jsp";
-	}
-	else if(action.equalsIgnoreCase("listarAdmin")) 
-	{
-		request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
-		acceso="listarProductosAdmin.jsp";
-	}
-	else if(action.equalsIgnoreCase("alta")) 
-	{
-		request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
-		acceso="altaProducto.jsp";
-	}
 		
-		RequestDispatcher vista = request.getRequestDispatcher(acceso);
-		vista.forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String acceso = "";
-		String action = request.getParameter("accion");
-		
-		if(action.equalsIgnoreCase("Agregar")) 
+		if (action == null) 
 		{
-			request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
-			Part imagen = request.getPart("imagen");
-			InputStream imagenInputStream = null;
-			try {
-				_validatorProducto.validacion_producto(request.getParameter("nombre"), request.getParameter("codigo_categoria"), request.getParameter("stock"), request.getParameter("precio"), imagen, null);
-				imagenInputStream = imagen.getInputStream();
-				Double precio = Double.parseDouble(request.getParameter("precio"));
-				prod = new Producto(request.getParameter("nombre"), imagenInputStream, Integer.parseInt(request.getParameter("stock")), Integer.parseInt(request.getParameter("codigo_categoria")));
-				_servicioProducto.AltaProducto(prod, precio);
-				request.setAttribute("mensajeOk","Alta realizada con éxito");
-			}
-			catch(AppException e){
-				request.setAttribute("mensajeError", e.getMessage());
-				request.setAttribute("nombre", request.getParameter("nombre"));
-				request.setAttribute("cantidad", request.getParameter("stock"));
-				request.setAttribute("precio", request.getParameter("precio"));
-				request.setAttribute("imagen", imagen);
-			}
-			catch (Exception e)
-			{
-				request.setAttribute("mensajeError","Error interno del servidor al realizar el alta");
-			}
-			acceso = "altaProducto.jsp";
+			acceso = "index.jsp";
 		}
-		else if(action.equalsIgnoreCase("EditarProducto")) 
+		if(action.equalsIgnoreCase("mostrar_producto")) 
 		{
-			Part imagen = request.getPart("imagen");
-			InputStream ImagenInputStream;
-			try
-			{
-				_validatorProducto.validacion_producto(request.getParameter("nombre"), null, null, request.getParameter("precio"), null, null);
-				ImagenInputStream = (imagen.getSize() != 0)?imagen.getInputStream():null;
-				prod = new Producto(Integer.parseInt(request.getParameter("codigo_producto")), request.getParameter("nombre"), ImagenInputStream, Double.parseDouble(request.getParameter("precio")));
-				_servicioProducto.EditarProducto(prod);
-				request.setAttribute("mensajeOk", "Modificación realizada con éxito");
-			}
-			catch (AppException e) {
-				request.setAttribute("mensajeError", e.getMessage());
-			}
-			catch (Exception e)
-			{
-				request.setAttribute("mensajeError", "Error interno del servidor");
-			}
-			acceso="editarProducto.jsp";
-		}
-		
-		else if(action.equalsIgnoreCase("BuscarProductoEditar")) 
-		{
+			Producto prod;
 			try
 			{
 				prod = _servicioProducto.GetProducto(Integer.parseInt(request.getParameter("codigo_producto")));
@@ -137,44 +57,164 @@ public class ControladorProducto extends HttpServlet {
 			{
 				request.setAttribute("mensajeError", "Error interno del servidor");
 			}
-			acceso="editarProducto.jsp";		
+			acceso = "producto.jsp";
+		}
+		else if(action.equalsIgnoreCase("listarCliente")) 
+		{
+			request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
+			acceso="listarProductos.jsp";
+		}
+		else if(action.equalsIgnoreCase("listarAdmin")) 
+		{
+			request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
+			acceso="listarProductosAdmin.jsp";
+		}
+		else if(action.equalsIgnoreCase("alta")) 
+		{
+			request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
+			acceso="altaProducto.jsp";
+		}
+			
+			RequestDispatcher vista = request.getRequestDispatcher(acceso);
+			vista.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String acceso = "";
+		String action = request.getParameter("accion");
+		HttpSession sesion = request.getSession(true);
+		if(action.equalsIgnoreCase("Agregar")) 
+		{
+			if (sesion.getAttribute("usuario_admin") != null)
+			{
+				request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
+				Part imagen = request.getPart("imagen");
+				InputStream imagenInputStream = null;
+				try {
+					_validatorProducto.validacion_producto(request.getParameter("nombre"), request.getParameter("codigo_categoria"), request.getParameter("stock"), request.getParameter("precio"), imagen, null);
+					imagenInputStream = imagen.getInputStream();
+					Double precio = Double.parseDouble(request.getParameter("precio"));
+					prod = new Producto(request.getParameter("nombre"), imagenInputStream, Integer.parseInt(request.getParameter("stock")), Integer.parseInt(request.getParameter("codigo_categoria")));
+					_servicioProducto.AltaProducto(prod, precio);
+					request.setAttribute("mensajeOk","Alta realizada con éxito");
+				}
+				catch(AppException e){
+					request.setAttribute("mensajeError", e.getMessage());
+					request.setAttribute("nombre", request.getParameter("nombre"));
+					request.setAttribute("cantidad", request.getParameter("stock"));
+					request.setAttribute("precio", request.getParameter("precio"));
+					request.setAttribute("imagen", imagen);
+				}
+				catch (Exception e)
+				{
+					request.setAttribute("mensajeError","Error interno del servidor al realizar el alta");
+				}
+				acceso = "altaProducto.jsp";
+			}
+			else
+			{
+				acceso = "loginAdmin.jsp";
+			}
+		}
+		else if(action.equalsIgnoreCase("EditarProducto"))
+		{
+			if (sesion.getAttribute("usuario_admin") != null)
+			{
+				Part imagen = request.getPart("imagen");
+				InputStream ImagenInputStream;
+				try
+				{
+					_validatorProducto.validacion_producto(request.getParameter("nombre"), null, null, request.getParameter("precio"), null, null);
+					ImagenInputStream = (imagen.getSize() != 0)?imagen.getInputStream():null;
+					prod = new Producto(Integer.parseInt(request.getParameter("codigo_producto")), request.getParameter("nombre"), ImagenInputStream, Double.parseDouble(request.getParameter("precio")));
+					_servicioProducto.EditarProducto(prod);
+					request.setAttribute("mensajeOk", "Modificación realizada con éxito");
+				}
+				catch (AppException e) {
+					request.setAttribute("mensajeError", e.getMessage());
+				}
+				catch (Exception e)
+				{
+					request.setAttribute("mensajeError", "Error interno del servidor");
+				}
+				acceso="editarProducto.jsp";
+			}
+			else
+			{
+				acceso = "loginAdmin.jsp";
+			}
+		}
+		
+		else if(action.equalsIgnoreCase("BuscarProductoEditar"))
+		{
+			if (sesion.getAttribute("usuario_admin") != null)
+			{
+				try
+				{
+					prod = _servicioProducto.GetProducto(Integer.parseInt(request.getParameter("codigo_producto")));
+					request.setAttribute("producto", prod);
+				}
+				catch (Exception e)
+				{
+					request.setAttribute("mensajeError", "Error interno del servidor");
+				}
+				acceso="editarProducto.jsp";
+			}
+			else
+			{
+				acceso = "loginAdmin.jsp";
+			}
 		}
 		
 		else if(action.equalsIgnoreCase("BajaProducto")) 
 		{
-			try
+			if (sesion.getAttribute("usuario_admin") != null)
 			{
-				_servicioProducto.EliminarProducto(Integer.parseInt(request.getParameter("codigo_producto_baja")));
-				request.setAttribute("mensajeOk", "Producto dado de baja con éxito");
+				try
+				{
+					_servicioProducto.EliminarProducto(Integer.parseInt(request.getParameter("codigo_producto_baja")));
+					request.setAttribute("mensajeOk", "Producto dado de baja con éxito");
+				}
+				catch (Exception e)
+				{
+					request.setAttribute("mensajeError", "Error interno del servidor");
+				}
+				request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
+				acceso = "listarProductosAdmin.jsp";
 			}
-			catch (Exception e)
+			else
 			{
-				request.setAttribute("mensajeError", "Error interno del servidor");
+				acceso = "loginAdmin.jsp";
 			}
-			request.setAttribute("categorias", _servicioCategoria.obtenerTodas());
-			acceso = "listarProductosAdmin.jsp";
 		}
 		
 		else if(action.equalsIgnoreCase("ActualizarStock")) 
 		{
-			try
+			if (sesion.getAttribute("usuario_admin") != null)
 			{
-				_validatorProducto.validacion_producto(null, null, request.getParameter("stock"), request.getParameter("precio"), null, request.getParameter("codigo_producto"));
-				_servicioProducto.ReponerStock(Integer.parseInt(request.getParameter("codigo_producto")), Integer.parseInt(request.getParameter("stock")), Double.parseDouble(request.getParameter("precio")));
-				request.setAttribute("mensajeOk", "Stock actualizado con éxito");
+				try
+				{
+					_validatorProducto.validacion_producto(null, null, request.getParameter("stock"), request.getParameter("precio"), null, request.getParameter("codigo_producto"));
+					_servicioProducto.ReponerStock(Integer.parseInt(request.getParameter("codigo_producto")), Integer.parseInt(request.getParameter("stock")), Double.parseDouble(request.getParameter("precio")));
+					request.setAttribute("mensajeOk", "Stock actualizado con éxito");
+				}
+				catch(AppException e) 
+				{
+					request.setAttribute("cantidad", request.getParameter("stock"));
+					request.setAttribute("precio", request.getParameter("precio"));
+					request.setAttribute("mensajeError", e.getMessage());
+				}
+				catch (Exception e)
+				{
+					request.setAttribute("mensajeError", "Error al actualizar el stock, error interno del servidor");
+				}
+				
+				acceso="actualizarStock.jsp";
 			}
-			catch(AppException e) 
+			else
 			{
-				request.setAttribute("cantidad", request.getParameter("stock"));
-				request.setAttribute("precio", request.getParameter("precio"));
-				request.setAttribute("mensajeError", e.getMessage());
+				acceso = "loginAdmin.jsp";
 			}
-			catch (Exception e)
-			{
-				request.setAttribute("mensajeError", "Error al actualizar el stock, error interno del servidor");
-			}
-			
-			acceso="actualizarStock.jsp";
 		}
 		else if(action.equalsIgnoreCase("listar") || action.equalsIgnoreCase("listarParaBajaCat")) 
 		{	
@@ -189,7 +229,6 @@ public class ControladorProducto extends HttpServlet {
 			{
 				request.setAttribute("mensajeError", "Error interno del servidor al intentar obtener los productos de la categoría seleccionada");
 			}
-			HttpSession sesion = request.getSession(true);
 			String usuario_admin = (String)sesion.getAttribute("usuario_admin");
 			if (action.equalsIgnoreCase("listar"))
 			{
