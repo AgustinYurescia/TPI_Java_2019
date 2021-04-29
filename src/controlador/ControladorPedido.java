fba58e4fa344ad02fed8746422ded09f39280564
Gianluca Gatti Teixeira <gianluk97@gmail.com>
2021-04-29 19:47:37 -0300

    more bug fix

--- src/controlador/ControladorPedido.java
+++ src/controlador/ControladorPedido.java
@@ -328,17 +328,21 @@ public class ControladorPedido extends HttpServlet {
 					int numeroPedido = Integer.parseInt(request.getParameter("numero_pedido"));
 					_servicioPedido.RegistrarEntrega(numeroPedido);
 					request.setAttribute("mensajeOk", "Entrega registrada con éxito");
+					acceso = "ControladorPedido?accion=listadoPedidos";
 				}
 				catch(AppException e)
 				{
 					request.setAttribute("mensajeError", e.getMessage());
+					acceso = "error.jsp";
 				}
 				catch (Exception e)
 				{
 					request.setAttribute("mensajeError", "Error interno del servidor");
+					acceso = "error.jsp";
 				}
+			}else {
+				acceso = "loginAdmin.jsp";
 			}
-			acceso = "ControladorPedido?accion=listadoPedidos";
 		}
 		
 		RequestDispatcher vista = request.getRequestDispatcher(acceso);
