cba443880e8a4a32a3afe3a73a5dd7e5487af0d6
Gianluca Gatti <gianlucagattiteixeira@gmail.com>
2024-10-30 00:14:10 -0300

    connection string for hosting

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -8,11 +8,11 @@ public class Conexion {
 	private int conectados=0;
 //	private String url="jdbc:mysql://node71920-elviejotonel.jelastic.saveincloud.net:3306/vinoteca_gatti?useTimezone=true&serverTimezone=UTC";
 	// private String url="jdbc:mysql://localhost:3306/vinoteca_gatti_2024?useTimezone=true&serverTimezone=America/Argentina/Buenos_Aires";
-	private String url="mysql://root:EqkXsWVVRSuLrMQdCIaZsVshjOtxqjbh@mysql.railway.internal:3306/railway";
 	// private String usuario="root";
 	// private String pass="root";
-	private String usuario="root";
-	private String pass="EqkXsWVVRSuLrMQdCIaZsVshjOtxqjbh";
+	private String url = "jdbc:mysql://autorack.proxy.rlwy.net:49938/railway";
+	private String usuario = "root";
+	private String pass = "EqkXsWVVRSuLrMQdCIaZsVshjOtxqjbh";
 	
 	
 	private Conexion() {
